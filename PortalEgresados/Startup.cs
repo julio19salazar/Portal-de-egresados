62dd9647025c765c81c679dc0aa72859e20dfbac
julio19salazar <juliosalazar378@gmail.com>
2022-02-28 09:08:11 -0600

    Carpetas y servicios MVC JULIO .

--- PortalEgresados/Startup.cs
+++ PortalEgresados/Startup.cs
@@ -16,6 +16,9 @@ namespace PortalEgresados
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //servicios añadidos JULIO SALAZAR
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
