b8055dce5df92777752e1aa4b96c5478590c5fbd
julio19salazar <juliosalazar378@gmail.com>
2022-03-03 08:25:52 -0600

    Patrón de repositorios.

---
+++ PortalEgresados/Repositories/Repository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalEgresados.Repositories
+{
+    public class Repository<T> where T : class
+    {
+        public DbContext Context { get; set; }
+
+        public Repository(DbContext context)
+        {
+            Context = context;
+        }
+
+        public virtual IEnumerable<T> GetAll()
+        {
+            return Context.Set<T>();
+        }
+
+        public virtual T Get(object id)
+        {
+            return Context.Find<T>(id);
+        }
+
+        public virtual void Insert(T entity)
+        {
+            Context.Add(entity);
+            Context.SaveChanges();
+        }
+
+
+        public virtual void Update(T entity)
+        {
+            Context.Update(entity);
+            Context.SaveChanges();
+        }
+
+        public virtual void Delete(T entity)
+        {
+            Context.Remove(entity);
+            Context.SaveChanges();
+        }
+
+
+        public virtual bool IsValid(T entity, out List<string> validationErrors)
+        {
+            validationErrors = new List<string>();
+            return true;
+        }
+    }
+}
