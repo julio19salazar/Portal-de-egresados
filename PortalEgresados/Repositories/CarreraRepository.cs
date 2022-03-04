9783a3127549acb96bd1f58d440d6af057bb7b1c
julio19salazar <juliosalazar378@gmail.com>
2022-03-04 07:30:44 -0600

    CarreraController en la rama hija

---
+++ PortalEgresados/Repositories/CarreraRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortalEgresados.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalEgresados.Repositories
+{
+    public class CarreraRepository : Repository<Carrera>
+    {
+        public CarreraRepository(DbContext context) : base(context)
+        {
+
+        }
+        public override IEnumerable<Carrera> GetAll()
+        {
+            return base.GetAll().Where(x=>x.Eliminado==0).OrderBy(x=>x.Nombre);
+        }
+    }
+}
